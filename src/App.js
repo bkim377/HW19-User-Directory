06d1cd8ee4fbb775aca00adfffa4f99b6d0ce31f
Brandon Kim <bkim377@gatech.edu>
2020-05-02 09:19:43 -0400

    started on HTML layout in app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import React from "react";
-import NavBar from "./components/Shared/NavBar";
+import React, {Component} from "react";
+import Table from "./components/Table";
+import employees from "./employees.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+class App extends Component {
+  // initial setup of conditions
+  state = {
+    employees, 
+    employeesToShow: [],
+    searchWord: "",
+  };
+
+  // Reach searchWord input
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  // Filtering employee names with searchWord
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const employees = this.state.employees.filter((employee) => {
+      return employee.name.includes(this.state.searchWord);
+    });
+    this.setState({
+      employees
+    });
+  };
+
+  
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row justify-content-end">
+          <form onSubmit={this.handleSubmit}>
+            <div className="input-group mb-3 searchBar">
+              <input 
+              type="text"
+              name="searchWord"
+              value={this.state.searchWord}
+              onChange={this.handleChange}
+              className="form-control"
+              placeholder="Enter Employee name"
+              />
+              <button type="submit"><i class="fa fa-search"></i></button>
+            </div>
+          </form>
+        </div>
+      </div>
+    )
+  }
+};
+
+
 
 export default App;
